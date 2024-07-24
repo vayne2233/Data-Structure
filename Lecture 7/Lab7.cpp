2125112d81ca69073836cef41e491c27807dca6c
vayne2233 <aeric9210151@gmail.com>
2024-07-25 02:32:59 +0800

    Update Lab7.cpp

--- Lecture 7/Lab7.cpp
+++ Lecture 7/Lab7.cpp
@@ -6,71 +6,74 @@ using namespace std;
 
 int main()
 {
-   int n;
+    int n;
 
-   list<int> mylist;
+    list<int> mylist;
 
-   cin >> n;
+    cin >> n;
 
-   while ( n-- )
-   {
-       int data;
-       string input;
-       cin >> input;
-       if ( input[0] == 'I' )
-       {
-           cin >> data;
+    while (n--)
+    {
+        int data;
+        string input;
+        cin >> input;
+        if (input[0] == 'I')
+        {
+            cin >> data;
 
-           if ( input[1] == 'H' )
-               mylist.push_front(data);
-           else if ( input[1] == 'T' )
-               mylist.push_back(data);
-       }
-       else if ( input[0] == 'R' )
-       {
-           if ( mylist.empty() ) continue;
+            if (input[1] == 'H')
+                mylist.push_front(data);
+            else if (input[1] == 'T')
+                mylist.push_back(data);
+        }
+        else if (input[0] == 'R')
+        {
+            if (mylist.empty())
+                continue;
 
-           else if ( input[1] == 'H' )
-               mylist.pop_front();
-           else if ( input[1] == 'T' )
-               mylist.pop_back();
-       }
-       else if ( input[0] == 'S' )
-       {
-           cin >> data;
-           if ( mylist.empty() )
-           {
-               cout << "E\n";
-               continue;
-           }
+            else if (input[1] == 'H')
+                mylist.pop_front();
+            else if (input[1] == 'T')
+                mylist.pop_back();
+        }
+        else if (input[0] == 'S')
+        {
+            cin >> data;
+            if (mylist.empty())
+            {
+                cout << "E\n";
+                continue;
+            }
 
-           bool find = false;
-           for ( auto x : mylist ) 
-           {
-               if ( x == data )
-               {
-                   find = true;
-                   break;
-               }
-           }
-           cout << (find ? "Y" : "N") << "\n";
-       }
-       else if ( input[0] == 'O' )
-       {
-           if ( mylist.empty() )
-           {
-               cout << "E\n";
-               continue;
-           }
-           for ( list<int>::iterator i = mylist.begin(); i != mylist.end(); ++i)
-           {
-               if (i == mylist.end() - 1)
-                   cout << *i;
-               else
-                   cout << *i << " ";
-           }
-           cout << "\n";
-       }
+            bool find = false;
+            for (auto x : mylist)
+            {
+                if (x == data)
+                {
+                    find = true;
+                    break;
+                }
+            }
+            cout << (find ? "Y" : "N") << "\n";
+        }
+        else if (input[0] == 'O')
+        {
+            if (mylist.empty())
+            {
+                cout << "E\n";
+                continue;
+            }
+
+            int cnt = 1;
+            for (auto x : mylist)
+            {
+                cout << x;
+                if (cnt == mylist.size())
+                    cout << "\n";
+                else
+                    cout << " ";
+                cnt++;
+            }
+        }
     }
 }
-
