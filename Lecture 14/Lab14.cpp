5028e4e52f8196bda2fe33c9d165469091703176
vayne2233 <aeric9210151@gmail.com>
2024-08-19 19:06:55 +0800

    Lab14 finish

--- Lecture 14/Lab14.cpp
+++ Lecture 14/Lab14.cpp
@@ -3,28 +3,46 @@
 #include <queue>
 using namespace std;
 
-int bfs( int n, vector<vector<int>> &arr, vector<int> &cookie, int st )
+int bfs(int n, const vector<vector<int>> &adj, vector<int> &ed, int st)
 {
     queue<int> q;
     vector<int> dis(n, -1);
     vector<int> in(n);
-    for ( int i : cookie ) in[i] = 1;
+    for (int i : ed)
+        in[i] = 1;
     dis[st] = 0;
     q.push(st);
-    while ( !q.empty() )
+    while (!q.empty())
     {
-
+        int u = q.front();
+        q.pop();
+        if (in[u])
+            return dis[u];
+        for (int v : adj[u])
+        {
+            if (dis[v] == -1)
+            {
+                dis[v] = dis[u] + 1;
+                q.push(v);
+            }
+        }
+    }
+    return -1;
+}
 
 int main()
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     int t;
     cin >> t;
-    while ( t-- )
+    while (t--)
     {
         int n, m;
         cin >> n >> m;
         vector<vector<int>> adj_list(n);
-        for ( int i = 0; i < m; i++ )
+        for (int i = 0; i < m; i++)
         {
             int u, v;
             cin >> u >> v;
@@ -36,15 +54,17 @@ int main()
         vector<int> cookie;
         cin >> s;
         cin >> sz;
-        while ( sz-- )
+        while (sz--)
         {
             int x;
             cin >> x;
             x--;
             cookie.emplace_back(x);
         }
-        int res = bfs( n, adj_list, cookie, s - 1 );
-        if ( res == -1 ) cout << "SAD\n";
-        else cout << res << "\n";
+        int res = bfs(n, adj_list, cookie, s - 1);
+        if (res == -1)
+            cout << "SAD\n";
+        else
+            cout << res << "\n";
     }
 }
